274b985da876cae1a9c5ea63c3a5e087f0a14dc7
iamonuwacj <onuwachinedu66@gmail.com>
2025-07-30 17:16:52 +0100

    added settings page

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div>
         <Sidebar />
-        <main className='md:ml-64 px-10 md:mt-[7.8rem] w-3/4'>
+        <main className='md:ml-64 px-10 md:mt-[7.8rem] md:w-3/4 w-full mt-32'>
             <h1 className='text-[20px] font-semibold'>Login Information</h1>
             <div className='flex md:w-md items-center justify-between mt-20'>
               <span className='text-[16px]'>Email</span> <span className='text-[#00000099]'>phweb3connect@gmail.com</span>
@@ -16,8 +16,8 @@ const page = () => {
             <div className='mt-16'>
               <h2 className='text-[20px]'>Delete account</h2>
 
-              <div className='border-[#FF0000] border-2 mt-10 text-[#FF0000] p-5 rounded-md flex justify-between'>
-                <div>
+              <div className='border-[#FF0000] border mt-10 text-[#FF0000] p-5 rounded-md flex md:flex-row flex-col justify-between'>
+                <div className='mb-5 md:mb-0'>
                   <p>Delete your account</p>
                   <span className='text-[14px]'>Delete and close your account.</span>
                 </div>
