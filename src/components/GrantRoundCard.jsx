184c2e501c85b180433f8c472e671a2e950b6e6f
Your Name Here <lawsonezekiel68@gmail.com>
2025-08-20 01:44:38 +0100

    feat: update grant rounds page, improve GrantRoundCard and Sidebar; add new assets except disbursed icon

--- src/components/GrantRoundCard.jsx
+++ src/components/GrantRoundCard.jsx
@@ -8,15 +8,16 @@ const GrantRoundCard = ({ grants }) => {
         {grants.map((item, index) => (
           <div
             key={index}
-            className="flex flex-col justify-between h-full w-full max-w-[360px] border border-gray-200 rounded-lg shadow-sm bg-white hover:shadow-md transition"
+            className="flex flex-col justify-between h-full w-full max-w-[360px] border border-gray-200  shadow-sm bg-white hover:shadow-md transition"
           >
             {/* Top image */}
-            <div className="h-[56px] relative">
+            <div className="flex items-center justify-center w-full h-auto">
               <Image
                 src={item.image}
                 alt={item.title}
-                fill
-                className="object-cover object-top rounded-t-lg"
+                width={320}
+                height={149}
+                className="object-contain lg:py-3 "
               />
             </div>
             {/* Card content */}
@@ -24,38 +25,30 @@ const GrantRoundCard = ({ grants }) => {
 
               <div>
                 <div className="flex justify-between items-center mb-2">
-                  <h3 className="text-[16px] text-black font-semibold leading-snug">
+                  <h3 className="text-[16px] text-[#000000] font-bold leading-snug">
                     {item.title}
                   </h3>
-                  <span className="text-[12px] font-medium rounded-full px-3 py-1 bg-[#D1F9E5] text-[#009951]">
-                    {item.status}
-                  </span>
+                  <p className="text-[10px] text-[#198038] font-sans font-bold border bg-[#FFFFFF] border-black/15 rounded px-3">{item.date}</p>
                 </div>
-                <div className="flex justify-between items-end mt-1 mb-3">
-                  <span className="inline-block bg-black text-white text-[12px] font-bold font-sans rounded px-2 py-1">
-                    {item.network}
-                  </span>
-                  <p className="text-[12px] text-[#198038] font-sans">{item.date}</p>
-                </div>
-
 
                 <p className="text-[14px] font-sans leading-snug text-gray-700">
                   {item.desc}
                 </p>
               </div>
               <div className="flex-grow"></div>
-              <p className="inline-flex items-center border bg-[#D8D8D833] border-black/20 rounded py-1 px-3 gap-1 text-[14px] font-sans font-medium mt-4 w-fit">
-                <span className="text-black font-extrabold">{item.amount}</span>
-                <span className="text-black/50">{item.coin}</span>
+              <p className="inline-flex items-center border bg-[#FFFFFF] border-black/15 text-[#198038] rounded  px-3 gap-1 text-[14px] font-sans font-medium mt-4 w-fit">
+                <span className=" font-extrabold">{item.amount}</span>
+                <span className="">{item.coin}</span>
               </p>
             </div>
-            <div className="px-5 py-3 border-t border-gray-200 mt-auto">
+            <div className="px-5 py-3  mt-auto">
               {item.link && (
                 <Link
                   href={item.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="block font-sans bg-[#198038] hover:bg-[#198038] transition rounded-md w-full py-3 text-center text-white text-sm font-medium"
+                  style={{ fontSize: '16px' }}
+                  className="block font-sans bg-[#A6E7D8]/50 border-1 border-[#008767] text-[#008767] transition  w-full py-3 text-center font-medium "
                 >
                   Apply
                 </Link>
